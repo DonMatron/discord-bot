c0fcf11e0b3f239534d616d8c4d1bae800e93cd3
Danylo Prudkyi <endermatrix.mmos@gmail.com>
2024-01-22 00:02:40 +0100

    Init service

---
+++ CoreService/Program.cs
@@ -0,0 +1,17 @@
+namespace CoreService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
