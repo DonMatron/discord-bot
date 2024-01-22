e7e679ca317f333a83af19097554373966730d50
Danylo Prudkyi <endermatrix.mmos@gmail.com>
2024-01-22 13:13:41 +0100

    Added logger

---
+++ CoreService/LoggingService.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+
+namespace CoreService
+{
+    public class LoggingService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public LoggingService(DiscordSocketClient client, CommandService command, ILogger<Worker> logger)
+        {
+            _logger = logger;
+            client.Log += LogAsync;
+            command.Log += LogAsync;
+        }
+
+        private Task LogAsync(LogMessage message)
+        {
+            if (message.Exception is CommandException cmdException)
+            {
+                _logger.LogInformation($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
+                    + $" failed to execute in {cmdException.Context.Channel}.");
+                _logger.LogInformation(cmdException.ToString());
+            }
+            else
+                _logger.LogInformation($"[General/{message.Severity}] {message}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- CoreService/Worker.cs
+++ CoreService/Worker.cs
@@ -1,21 +1,44 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
 namespace CoreService
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+
+        private readonly DiscordSocketClient _client;
+
+        private readonly CommandService _commandService;
+
+        private readonly LoggingService _loggingService;
 
         public Worker(ILogger<Worker> logger)
         {
-            _logger = logger;
+            _client = new DiscordSocketClient();
+            _commandService = new CommandService();
+            _loggingService = new LoggingService(_client, _commandService, logger);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            //TODO: move token to safer place
+            var token = "MTE5ODc1NzI2ODQ4MTYzODU1MQ.GCNjdu.vZlEfS0GKw5-yESdMLV-RxbEf04I3FGVnwvKiE";
+
+            //Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            //var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            //var token = File.ReadAllText("token.txt");
+            //var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                // Block this task until the program is closed.
+                await Task.Delay(-1);
             }
+
         }
     }
 }
